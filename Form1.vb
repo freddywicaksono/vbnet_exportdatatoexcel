2f8cd4106d6cb11d0901b2095753343d8475a27e
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2021-01-21 23:00:33 +0700

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports ExcelLib.Client
+Public Class Form1
+    Private xls As New ExcelLib.Client
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Reload()
+    End Sub
+
+    Private Sub Reload()
+        ofakultas.GetAllData(DataGridView1)
+    End Sub
+
+
+    Private Sub btnExport_Click(sender As System.Object, e As System.EventArgs) Handles btnExport.Click
+        'ExportToExcel("c:\data\laporanDataFakultas.xlsx", "Sheet1", DataGridView1)
+        xls.FileName = "c:\data\laporanDataFakultas2.xlsx"
+        xls.SheetName = "Sheet1"
+        xls.Visible = False
+        xls.ExportToExcel(DataGridView1)
+    End Sub
+End Class
---
+++ MyMod.vb
@@ -0,0 +1,30 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module MyMod
+    Public mycommand As New MySqlCommand
+    Public myadapter As New MySqlDataAdapter
+    Public mydata As New System.Data.DataTable
+    Public ds As New DataSet
+    Public DR As MySql.Data.MySqlClient.MySqlDataReader
+    Public SQL As String
+    Public conn As New MySql.Data.MySqlClient.MySqlConnection
+    Public cn As New Connection
+    
+    'Tabel Fakultas
+    '-------------------------------
+    Public fakultas_baru As Boolean
+    Public ofakultas As New Fakultas
+    '--------------------------------
+
+    Public Sub DBConnect()
+        cn.Host = "localhost"
+        cn.User = "root"
+        cn.Password = ""
+        cn.DatabaseName = "dbumc"
+        cn.Connect()
+    End Sub
+
+    Public Sub DBDisconnect()
+        cn.Disconnect()
+    End Sub
+End Module
